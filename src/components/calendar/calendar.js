5d133bb419555d5e57cf9fe475a5b3696ce9d228
arranz88 <laura.arranz@vass.es>
2019-03-27 17:05:09 +0100

    Style title calendar

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -1,33 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 
+import './calendar.scss'
+
 class CalendarMood extends Component {
 
     render() {
         const { listMood } = this.props;
 
-        if (listMood === null) {
-            return <div>cargando</div>
-        } else {
-            return (
-                <div>
-                <Link to="/edit">
-                    <button>+</button>
-                </Link>
-                    <ul>
-                        {listMood.map((itme, i) => {
-                            return (
-                                <li key={i}>
-                                    {itme.mood}
-                                    {itme.message}
-                                </li>
-                            )
-                        })}
-                    </ul>
-                </div>
-            )
-        }
-
+        return (
+            <div className="calendar-container">
+            <div>
+               <h1 className="calendar-title">¿cómo te sientes hoy?</h1>
+            <Link to="/edit">
+                <button className="calendar-button">+</button>
+            </Link> 
+            </div>
+            
+                <ul className="calendar-list">
+                    {listMood.map((itme, i) => {
+                        return (
+                            <li key={i} className="calendar-item">
+                                {itme.mood}
+                                {itme.message}
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
     }
 }
 
