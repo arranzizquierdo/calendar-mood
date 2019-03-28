d91e5b400d5cb981a1369626c8fac2af070f166d
arranz88 <laura.arranz@vass.es>
2019-03-28 15:18:04 +0100

    Try publish app

---
+++ docs/precache-manifest.cfddbd7c7b4e4595f97460d21cee70e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9ccc775b2eb2be49a9a",
+    "url": "/static/css/main.a0f1dbe3.chunk.css"
+  },
+  {
+    "revision": "f9ccc775b2eb2be49a9a",
+    "url": "/static/js/main.8bf78c85.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e9e29dedd676d9d9770f",
+    "url": "/static/js/2.9db2cda3.chunk.js"
+  },
+  {
+    "revision": "de402805740383e34ba520f1bd320008",
+    "url": "/static/media/bg-white.de402805.jpeg"
+  },
+  {
+    "revision": "9afc98d687f1aa01201e84e5edc9d026",
+    "url": "/index.html"
+  }
+];
