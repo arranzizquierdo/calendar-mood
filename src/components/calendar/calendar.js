9d74e10acd4e8039c88b5d10160f7a33502b4a0e
arranz88 <laura.arranz@vass.es>
2019-03-28 11:38:15 +0100

    Fix show tooltip

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -1,47 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-import './calendar.scss'
+import "./calendar.scss";
 
 class CalendarMood extends Component {
-
-    showTooltip(mood){
-        if(mood === ":)") {
-            return ""
-        } else {
-            return "hidden"
-        }
+  showTooltip(message) {
+    if (message !== "") {
+      return true;
+    } else {
+      return false;
     }
-    render() {
-        const { listMood } = this.props;
+  }
+  render() {
+    const { listMood } = this.props;
 
-        return (
-            <div className="calendar-container">
-            <div>
-               <h1 className="calendar-title">¿cómo te sientes hoy?</h1>
-            <Link to="/edit">
-                <button className="calendar-button">+</button>
-            </Link> 
-            </div>
-            
-                <ul className="calendar-list">
-                    {listMood.map((item, i) => {
-                        return (
-                            <li key={i} className="calendar-item tooltip">
-                                {item.mood}
-                                <div className={`${this.showTooltip(item.mood)}`}>
-                                    <span className="tooltiptext">
-                                    {item.message}
-                                </span>
-                                </div>
-                                
-                            </li>
-                        )
-                    })}
-                </ul>
-            </div>
-        )
-    }
+    return (
+      <div className="calendar-container">
+        <div>
+          <h1 className="calendar-title">¿cómo te sientes hoy?</h1>
+          <Link to="/edit">
+            <button className="calendar-button">+</button>
+          </Link>
+        </div>
+
+        <ul className="calendar-list">
+          {listMood.map((item, i) => {
+            return (
+              <li key={i} className="calendar-item tooltip">
+                {item.mood}
+                {this.showTooltip(item.message) ? (
+                  <span className="tooltiptext">{item.message}</span>
+                ) : null}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default CalendarMood;
